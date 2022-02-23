d3f8f05b601641bbfec4f0a88d3b41c2dca079e4
Joshua Smith <jcsmith2393@gmail.com>
2022-02-23 14:57:13 -0600

    small edits added the slider

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const priceInput=document.querySelector('[name=price]');
 const quantityInput=document.querySelector('[name=quantity]');
-const total=document.querySelector('.total');
+const total= document.querySelector('.total')
 
 
 priceInput.addEventListener('input', calculateItPrice);
+quantityInput.addEventListener('input', calculateItPrice)
 
 function calculateItPrice(){
-    console.log(priceInput.value)
+    const price =priceInput.value;
+    const quantity= quantityInput.value;
+    const cost = price*quantity;
+    console.log(total);
+    total.innerText='$'+ cost.toFixed(2)
 }
+
+
+calculateItPrice();
