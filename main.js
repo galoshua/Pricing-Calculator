71fd892ce68c4b3570d29e8ccee379b2a420bf01
Joshua Smith <jcsmith2393@gmail.com>
2022-02-23 15:10:18 -0600

    finished pricing calculator

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const priceInput=document.querySelector('[name=price]');
 const quantityInput=document.querySelector('[name=quantity]');
 const total= document.querySelector('.total')
-
+const quantityLabel =document.querySelector('.quantity-label')
 
 priceInput.addEventListener('input', calculateItPrice);
 quantityInput.addEventListener('input', calculateItPrice)
-
+quantityInput.addEventListener('input',updateQuantityLabel)
 function calculateItPrice(){
     const price =priceInput.value;
     const quantity= quantityInput.value;
     const cost = price*quantity;
     console.log(total);
-    total.innerText='$'+ cost.toFixed(2)
+    total.innerHTML='$'+ cost.toFixed(2);
 }
 
 
-calculateItPrice();
+function updateQuantityLabel(){
+    const quantity=quantityInput.value;
+    quantityLabel.innerText= quantity;
+}
